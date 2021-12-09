9fba7a39e85ae1a6705f0d04fb1ad952f7fa6f5c
Sonia Vecino <83987853+sonia-vecino@users.noreply.github.com>
2021-12-08 23:16:32 -0300

    Add files via upload

---
+++ Ejercicio-obligatorio-javascript-2/script.js
@@ -0,0 +1,27 @@
+
+
+for(i=11;i<=11;i+=11) {
+    document.write("<br> la tabla del " + i + ": "+"<br>");
+    for (j=1;j<10;j++){
+        document.write (" "+ i + " x "+ j + ": ")
+        document.write (i*j+"<br>")
+    }
+}
+
+for(i=10;i<=10;i+=10) {
+    document.write ("<br>  la tabla del " + i+ ": "+ "<br>");
+    for (j=1;j<10;j++){
+        document.write (" "+ i + " x "+ j + ": ")
+        document.write (i*j + "<br>")
+    }
+}
+
+
+for(i=9;i<=10;i+=9) {
+    document.write ("<br>  la tabla del " + i + ": "+ "<br>");
+    for (j=1;j<10;j++){
+        document.write (" " + i + " x "+ j + ": ")
+        document.write (i*j + "<br>")
+    }
+}
+
