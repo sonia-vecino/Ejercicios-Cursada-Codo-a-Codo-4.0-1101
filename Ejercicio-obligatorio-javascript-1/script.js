620944d7e4c55c5e8a9c7f2b8a61595b8d95ff42
Sonia Vecino <83987853+sonia-vecino@users.noreply.github.com>
2021-12-08 23:11:19 -0300

    Add files via upload

---
+++ Ejercicio-obligatorio-javascript-1/script.js
@@ -0,0 +1,10 @@
+let numUno=40;
+let numDos=16;
+let sumaTotal= numUno+numDos;
+
+console.log("El resultado de la suma de las dos variables es: " + sumaTotal);
+
+function addParagraphText()
+	{
+	document.getElementById("para").innerHTML="El resultado de la suma de las dos variables es: " + sumaTotal;
+	}
