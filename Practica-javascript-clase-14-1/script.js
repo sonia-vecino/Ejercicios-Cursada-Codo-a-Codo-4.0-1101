fcee7a2c52600dc9cc8fa82f02524399d27b56de
Sonia Vecino <83987853+sonia-vecino@users.noreply.github.com>
2021-12-08 23:54:53 -0300

    Add files via upload

---
+++ Practica-javascript-clase-14-1/script.js
@@ -0,0 +1,7 @@
+
+
+let name=prompt("ingrese su nombre");
+let fecha=prompt ("ingrese su año de nacimiento");
+let años= ( 2021 - fecha);
+console.log ("usted tiene "+ años+ " años");
+alert ("usted tiene "+ años + " años");
